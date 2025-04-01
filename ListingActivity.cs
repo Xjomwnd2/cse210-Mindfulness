4158754fdf65645497d58acac6015e9a46fd3199
YourGitHubUsername <jmwaura@byupathway.edu>
2025-04-02 00:36:46 +0300

    commit

---
+++ ListingActivity.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+
+public class ListingActivity : Activity
+{
+    private List<string> _prompts;
+    private int _count;
+
+    public ListingActivity() 
+        : base("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
+    {
+        _prompts = new List<string>
+        {
+            "Who are people that you appreciate?",
+            "What are personal strengths of yours?",
+            "Who are people that you have helped this week?",
+            "When have you felt the Holy Ghost this month?",
+            "Who are some of your personal heroes?"
+        };
+        _count = 0;
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+        
+        Console.WriteLine("List as many responses as you can to the following prompt:");
+        Console.WriteLine();
+        DisplayPrompt();
+        Console.WriteLine();
+        
+        Console.Write("You may begin in: ");
+        ShowCountDown(5);
+        Console.WriteLine();
+        
+        List<string> items = GetListFromUser();
+        _count = items.Count;
+        
+        Console.WriteLine($"You listed {_count} items!");
+        
+        DisplayEndingMessage();
+    }
+
+    private string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+
+    private void DisplayPrompt()
+    {
+        string prompt = GetRandomPrompt();
+        Console.WriteLine($"--- {prompt} ---");
+    }
+
+    private List<string> GetListFromUser()
+    {
+        List<string> items = new List<string>();
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(GetDuration());
+        
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            string item = Console.ReadLine();
+            items.Add(item);
+        }
+        
+        return items;
+    }
+}
